31c105529c6bcc111c24a21bb56e41f1c88f10d7
Connie <cleung334@gmail.com>
2015-08-09 22:30:34 +0800

    use  to obtain http or https protocol
    Signed-off-by: Connie <cleung334@gmail.com>

--- src/app/geocoder/geocoder.factory.js
+++ src/app/geocoder/geocoder.factory.js
@@ -7,15 +7,15 @@
  * Factory in the angularEventJourney.
  */
 angular.module('angularEventJourney')
-  .factory('geocoderFactory', function ($http, $q) {
+  .factory('geocoderFactory', function ($http, $q, $location) {
 
   	// use mapquest geocoding api
     var appKey = '0WGV61dvTFci9Fu8Liu9bmbnTGUz2f7b';
-    var hkboundingBox = "&boundingBox=22.153549, 113.835083,22.56204, 114.441788";
-    var geocodeUrl = "http://www.mapquestapi.com/geocoding/v1/address?key=" +  
+    var hkboundingBox = '&boundingBox=22.153549, 113.835083,22.56204, 114.441788';
+    var geocodeUrl = $location.protocol() + '://www.mapquestapi.com/geocoding/v1/address?key=' +  
               appKey + 
               hkboundingBox +
-    					"&inFormat=kvp&outFormat=json&maxResults=1&thumbMaps=false&location=";
+    					'&inFormat=kvp&outFormat=json&maxResults=1&thumbMaps=false&location=';
 
 // Public API here
     return {
@@ -37,12 +37,12 @@ angular.module('angularEventJourney')
             };
            return obj;
        },
-      getLatLng : function _getLatLng(address, objGeocode) {
+      getLatLng : function _getLatLng(address) {
       	var requestUrl = geocodeUrl + address;
       	var deferred = $q.defer();
       	$http.get(requestUrl).then(function (response) {
       		var latlng =response.data.results[0].locations[0].latLng;
-      		console.log ("lat: " + latlng.lat + ", lng: " + latlng.lng);
+      		console.log ('lat: ' + latlng.lat + ', lng: ' + latlng.lng);
       		var result = { 
             	lat: latlng.lat,
             	lng: latlng.lng,
